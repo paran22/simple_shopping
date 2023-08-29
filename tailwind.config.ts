eee00be0640d04eea54efe6b9bdf96a89e5068d7
gaeun <knospe1@naver.com>
2023-08-29 13:51:05 +0900

    Fix: fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     color: {
       'primary': '#1D5D9B',
