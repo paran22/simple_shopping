0ddfb009a07603a2fe10c161c49f6262af0af310
gaeun <knospe1@naver.com>
2023-08-29 14:14:27 +0900

    Fix: fix tailwind config

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './common/router/router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./common/router/router";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode> 
+  <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,tsx,ts}'],
+  theme:
+  {
+    color: {
+      'primary': '#1D5D9B',
+      'secondary': '#F4D160',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
